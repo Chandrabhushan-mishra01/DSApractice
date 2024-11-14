d377da10dfaaaac33c7fc18353cae550b12906db
ChandrabhushanMishra <chandrabhushanmishra900@gmail.com>
2024-11-14 15:22:40 +0530

    merge two sorted link list problem solved

--- ds.cpp
+++ ds.cpp
@@ -1072,4 +1072,41 @@ using namespace std;
 // 	} 
 // }
 
+node* sortedMerge(node* head1, node* head2) {
+    if (head1 == NULL) return head2;
+    if (head2 == NULL) return head1;
+
+    node* head = NULL;
+    node* prev = NULL;
+
+    if (head1->data < head2->data) {
+        head = head1;
+        prev = head1;
+        head1 = head1->next;
+    } else {
+        head = head2;
+        prev = head2;
+        head2 = head2->next;
+    }
+
+    while (head1 != NULL && head2 != NULL) {
+        if (head1->data <= head2->data) {
+            prev->next = head1;
+            prev = head1;
+            head1 = head1->next;
+        } else {
+            prev->next = head2;
+            prev = head2;
+            head2 = head2->next;
+        }
+    }
+
+    if (head1 == NULL) {
+        prev->next = head2;
+    } else {
+        prev->next = head1;
+    }
+
+    return head;
+}
 
