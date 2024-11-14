c6d68eb46e075ffab808108c980ee257307e9dfd
ChandrabhushanMishra <chandrabhushanmishra900@gmail.com>
2024-11-14 15:50:57 +0530

    Palindrome link list problem solved

--- ds.cpp
+++ ds.cpp
@@ -1072,41 +1072,76 @@ using namespace std;
 // 	} 
 // }
 
-node* sortedMerge(node* head1, node* head2) {
-    if (head1 == NULL) return head2;
-    if (head2 == NULL) return head1;
+// node* sortedMerge(node* head1, node* head2) {
+//     if (head1 == NULL) return head2;
+//     if (head2 == NULL) return head1;
 
-    node* head = NULL;
+//     node* head = NULL;
+//     node* prev = NULL;
+
+//     if (head1->data < head2->data) {
+//         head = head1;
+//         prev = head1;
+//         head1 = head1->next;
+//     } else {
+//         head = head2;
+//         prev = head2;
+//         head2 = head2->next;
+//     }
+
+//     while (head1 != NULL && head2 != NULL) {
+//         if (head1->data <= head2->data) {
+//             prev->next = head1;
+//             prev = head1;
+//             head1 = head1->next;
+//         } else {
+//             prev->next = head2;
+//             prev = head2;
+//             head2 = head2->next;
+//         }
+//     }
+
+//     if (head1 == NULL) {
+//         prev->next = head2;
+//     } else {
+//         prev->next = head1;
+//     }
+
+//     return head;
+// }
+
+node* reverseList(node* head) {
     node* prev = NULL;
-
-    if (head1->data < head2->data) {
-        head = head1;
-        prev = head1;
-        head1 = head1->next;
-    } else {
-        head = head2;
-        prev = head2;
-        head2 = head2->next;
+    node* curr = head;
+    node* next = NULL;
+    while (curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
     }
-
-    while (head1 != NULL && head2 != NULL) {
-        if (head1->data <= head2->data) {
-            prev->next = head1;
-            prev = head1;
-            head1 = head1->next;
-        } else {
-            prev->next = head2;
-            prev = head2;
-            head2 = head2->next;
+    return prev;
+}
+bool isPalindrome(node* head) {
+    if (head == NULL) return true;
+    node* slow = head;
+    node* fast = head;
+    while (fast->next != NULL && fast->next->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    node* rev = reverseList(slow->next);
+    node* curr = head;
+    node* tempRev = rev; 
+    while (rev != NULL) {
+        if (rev->data != curr->data) {
+            reverseList(tempRev);  
+            return false;
         }
+        rev = rev->next;
+        curr = curr->next;
     }
-
-    if (head1 == NULL) {
-        prev->next = head2;
-    } else {
-        prev->next = head1;
-    }
-
-    return head;
+    reverseList(tempRev);
+    return true;
 }
 
