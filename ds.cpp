69fb4b5888284408e08e3037e8acbe258358e4de
ChandrabhushanMishra <chandrabhushanmishra900@gmail.com>
2024-11-18 19:51:51 +0530

    stock span,previous greater , next greater has similar logic

--- ds.cpp
+++ ds.cpp
@@ -1468,19 +1468,19 @@ using namespace std;
 //     return ans;
 // }
 
-vector<int> nextGreater(vector<int>& arr) {
-    int n = arr.size();
-    stack<int> s;
-    vector<int> ans(n); 
-    for (int i = n - 1; i >= 0; i--) { 
-        while (!s.empty() && s.top() <= arr[i]) {
-            s.pop();
-        }
-        ans[i] = s.empty() ? -1 : s.top();
-        s.push(arr[i]);
-    }
-    return ans;
-}
+// vector<int> nextGreater(vector<int>& arr) {
+//     int n = arr.size();
+//     stack<int> s;
+//     vector<int> ans(n); 
+//     for (int i = n - 1; i >= 0; i--) { 
+//         while (!s.empty() && s.top() <= arr[i]) {
+//             s.pop();
+//         }
+//         ans[i] = s.empty() ? -1 : s.top();
+//         s.push(arr[i]);
+//     }
+//     return ans;
+// }
 
 
 
