58a026c5b99069d6785ccaa907d640abdfa7d4e5
ChandrabhushanMishra <chandrabhushanmishra900@gmail.com>
2024-12-25 12:36:42 +0530

    theory part of tree

--- treebstheap.cpp
+++ treebstheap.cpp
@@ -5,3 +5,29 @@ int main(){
     cout<<1<<endl;
     return 0;
 }
+
+
+// -------------------------------------------------tree-----------------------------------------------------------------
+// term: node, root, leaf, child, parent, subtree, descendants, ancestors, degree(no. of child ), internal
+// application: To represent hierarchical data (organization structure, folder structure, xml/html content(json object), in oop)
+//              binary search tree, binary heap, b ans b+ tree in DBMS, spanning and shortest path tree in computer network,
+//              parse tree, expression tree in compiler, trie, suffix tree, binary index tree, segment tree
+// Binary tree: every node has at most two children (1 or 2)                                           eg.  *l {30} *r
+//              every node has tree fields ---> [  [*left pointer] [key or data] [*right pointer]  ] :      /        \
+// c++ implementation to represent the binary tree 
+// struct node{
+//     int key;
+//     int *left;
+//     int *right;
+//     node(int k){
+//         key=k;
+//         left=NULL;
+//         right=NULL;
+//     }
+// }
+// int main(){
+//     node *root = new node(10);
+//     root->left = new node(20);
+//     root->right = new node(30);
+//     root->left->left = new node(40);
+// }
