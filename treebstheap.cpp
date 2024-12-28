ca3c729bfa0024dbce2252afb417445f3289199f
ChandrabhushanMishra <chandrabhushanmishra900@gmail.com>
2024-12-28 12:32:24 +0530

    tree traversal implemenetation

--- treebstheap.cpp
+++ treebstheap.cpp
@@ -34,10 +34,61 @@ using namespace std;
 
 // tree traversal: 1)breath first(or level order).  2) depth first - a)inorder b)preorder C)postorder
 // df traversal -          /  traverse root              there are factorial 3 way to do these task
-//              recursuve -   traversse left subtree     front 6 way three are famous :
+//              recursive -   traversse left subtree     front 6 way three are famous :
 //                         \  traverse right subtree     inorder preorder and postorder(in these three permutation we visit left subtree before right subtree).
 // inorder   - (left root right)
 // preorder  - (root left right)    //# left - left subtree //# right - right subtree
 // postorder - (left right root)
 // implementation : 
 
+struct node {
+    int key;
+    node *left;
+    node *right;
+    node(int k) {
+        key = k;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void inorder(node *root) {
+    if (root != NULL) {
+        inorder(root->left);
+        cout << root->key << " ";
+        inorder(root->right);
+    }
+}
+
+void preorder(node *root){
+    if(root != NULL){
+        cout << root->key << " ";
+        inorder(root->left);
+        inorder(root->right);
+    }
+}
+
+void postorder(node *root){
+    if(root != NULL){
+        inorder(root->right); 
+        inorder(root->left);
+        cout << root->key << " ";
+    }
+}
+int main() {
+    // Creating nodes
+    node *root = new node(10);
+    root->left = new node(20);
+    root->right = new node(30);
+    root->left->left = new node(40);
+    root->left->right = new node(50);
+    root->right->left = new node(60);
+    root->right->right = new node(70);
+
+    // Performing in-order traversal
+    cout << "Inorder traversal of the binary tree: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}
