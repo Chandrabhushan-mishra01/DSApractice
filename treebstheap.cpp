8acde2f0aecf9ca7e08f602b4a3bcd4d14f3830f
ChandrabhushanMishra <chandrabhushanmishra900@gmail.com>
2025-02-09 18:59:45 +0530

    find kth smallest in BST

--- treebstheap.cpp
+++ treebstheap.cpp
@@ -963,6 +963,50 @@ void printCeilingE(int arr[], int n){
 }
 
 
+struct Node  
+{ 
+  int key; 
+  struct Node *left; 
+  struct Node *right;
+  int lCount;
+  Node(int k){
+      key=k;
+      left=right=NULL;
+      lCount=0;
+  }
+};
+
+Node* insert(Node* root, int x)
+{
+    if (root == NULL)
+        return new Node(x);
+
+    if (x < root->key) {
+        root->left = insert(root->left, x);
+        root->lCount++;
+    }
+ 
+    else if (x > root->key)
+        root->right = insert(root->right, x);
+    return root;
+}
+
+Node* kthSmallest(Node* root, int k)
+{
+    if (root == NULL)
+        return NULL;
+ 
+    int count = root->lCount + 1;
+    if (count == k)
+        return root;
+ 
+    if (count > k)
+        return kthSmallest(root->left, k);
+ 
+    return kthSmallest(root->right, k - count);
+}
+
+
 int main() {
     node* root = nullptr;
 
