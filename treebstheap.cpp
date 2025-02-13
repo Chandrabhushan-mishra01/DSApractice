80e33cb89de7997f955ea3edd78fc7367954bd02
ChandrabhushanMishra <chandrabhushanmishra900@gmail.com>
2025-02-14 00:27:33 +0530

    basic operation in Binary heap(minHeap)

--- treebstheap.cpp
+++ treebstheap.cpp
@@ -1128,3 +1128,138 @@ sorted traversal    |   O(n log n)      |  O(n)         |    O(n log n)|   O(n) 
 
 //     return 0;
 // }
+
+// -----------------------------------------------Binary Heap--------------------------------------------------
+/*
+Notes: Binary heap is a complete binary tree(stored as an array).
+1.It is used in heap sort.
+2.used to implement Priority Queue.
+3.Two Types: 
+            MinHeap-highest priority item is assigned lowest value.(every node has value smaller than its descendants)
+            MaxHeap-highest priority item is assigned highest value.
+
+*/
+
+//Heap implementation
+
+class MinHeap {
+    int *arr;
+    int size;
+    int capacity;
+
+public:
+    MinHeap(int c) { 
+        arr = new int[c];  
+        size = 0;
+        capacity = c;
+    }
+
+    int left(int i) { 
+        return (2 * i + 1); 
+    }
+    int right(int i) { 
+        return (2 * i + 2); 
+    }
+    int parent(int i) { 
+        return (i - 1) / 2; 
+    }
+
+    void insert(int x) {
+        if (size == capacity) return;
+        size++;
+        arr[size - 1] = x;
+        int i = size - 1;
+        while (i != 0 && arr[parent(i)] > arr[i]) {
+            swap(arr[i], arr[parent(i)]);
+            i = parent(i);
+        }
+    }
+
+    void minHeapify(int i) {
+        int lt = left(i);
+        int rt = right(i);
+        int smallest = i;
+        
+        if (lt < size && arr[lt] < arr[i]) {
+            smallest = lt;
+        }
+        if (rt < size && arr[rt] < arr[smallest]) {
+            smallest = rt;
+        }
+        if (smallest != i) {
+            swap(arr[i], arr[smallest]);
+            minHeapify(smallest);
+        }
+    }
+
+    int extractMin() {
+        if (size == 0) return -1; 
+        if (size == 1) { 
+            size--;
+            return arr[0]; 
+        }
+        swap(arr[0],arr[size-1]);
+        size--;  
+        minHeapify(0);  
+
+        return arr[size];  
+    }
+
+    void decreaseKey(int i, int x){
+        arr[i]=x;
+        while(i!=0 && arr[parent(i)]>arr[i]){
+            swap(arr[i],arr[parent(i)]);
+            i=parent(i);
+        }
+    }
+
+    void deleteKey(int i){
+        if (i >= size) return;  
+        arr[i] = arr[size - 1];  
+        size--;  
+
+        if (i != 0 && arr[i] < arr[parent(i)]) {
+            while (i != 0 && arr[parent(i)] > arr[i]) {
+                swap(arr[i], arr[parent(i)]);
+                i = parent(i);
+            }
+        } else {
+            minHeapify(i);
+        }
+        
+    }
+    void buildHeap() {
+        for (int i = (size - 2) / 2; i >= 0; i--) {  //bottom most right most internal node -using parent formula 
+            minHeapify(i);
+        }
+    }
+
+    void printHeap() { 
+        for (int i = 0; i < size; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    MinHeap h(10);
+    
+    h.insert(10);
+    h.insert(35);
+    h.insert(15);
+    h.insert(30);
+    h.insert(40);
+    
+    cout << "Heap before extraction: ";
+    h.printHeap();
+
+    cout << "Extracted Min: " << h.extractMin() << endl;
+
+    cout << "Heap after extraction: ";
+    h.printHeap();
+
+    return 0;
+}
+
+
