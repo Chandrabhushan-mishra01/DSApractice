a0a2ac5a684f9fe5c039a74718b411fbcdadc4e6
ChandrabhushanMishra <chandrabhushanmishra900@gmail.com>
2025-06-11 10:54:31 +0530

    Tabulation's example

--- Dp.cpp
+++ Dp.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//memoization - TC-O(N) and SC-O(N)+O(N) (one is for stack space and other one is for array space)
 int f(int n, vector<int>& dp) {
     if (n <= 1) return n;
     if (dp[n] != -1) return dp[n];
@@ -8,10 +9,21 @@ int f(int n, vector<int>& dp) {
     return dp[n];
 }
 
+//tabulation - TC-O(N) and SC-O(N) (Remove stack space)
+int tf(int n){
+    int prev2=0;
+    int prev =1;
+    for(int i=2; i<=n;i++){
+        int curri = prev + prev2;
+        prev2 = prev;
+        prev = curri;
+    }
+    return prev;
+}
 int main() {
     int n;
     cin >> n;
     vector<int> dp(n + 1, -1);
-    cout << f(n, dp) << endl;
+    cout << tf(n) << endl;
     return 0;
 }
